23176561595e6e7c69066b451339565ad6432663
ygulve <38343976+ygulve@users.noreply.github.com>
2019-05-10 15:49:52 +0530

    Update in file

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class config {
 
-    private APIresult: string = "http://localhost:44393"
+    private APIresult: string = "https://localhost:44393"
 
     constructor() {
 
