9827c2ff5c378e9eefa5ddd5d02ba246b2802d1d
ygulve <38343976+ygulve@users.noreply.github.com>
2019-05-10 16:55:32 +0530

    Update file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import{HttpModule, XHRBackend, RequestOptions} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import{ LoginService} from './login/login.service';
 import { config } from "../config/config";
 import { FormsModule,ReactiveFormsModule   } from '@angular/forms';
-import { HttpInterceptor } from './http_interceptor';
-
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
+import { ToastrModule } from 'ngx-toastr';
+import { EmployeeService } from './employeelist/employee.service';
+import { TokenInterceptor } from './AuthService/token.interceptor';
+import {AuthService} from './AuthService/AuthService';
 @NgModule({
   declarations: [
     AppComponent,    
-    LoginComponent
+    LoginComponent, EmployeelistComponent
   ],
   imports: [
     BrowserModule,
@@ -20,16 +25,20 @@ import { HttpInterceptor } from './http_interceptor';
     ReactiveFormsModule,
     AppRoutingModule,
     HttpModule,
+    HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 1500,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }), // ToastrModule added,
   ],
-  providers: [config,LoginService,
+  providers: [config,LoginService,EmployeeService,AuthService,
         {
 
-          provide: HttpInterceptor,
-          useFactory:
-          (backend: XHRBackend, defaultOptions: RequestOptions) => {
-              return new HttpInterceptor(backend, defaultOptions);
-          },
-          deps: [XHRBackend, RequestOptions]
+          provide: HTTP_INTERCEPTORS,
+          useClass: TokenInterceptor,      
+          multi: true
+         
         }
   ],
   bootstrap: [AppComponent]
