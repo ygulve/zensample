54143ea14fb30dc7b72cfc345613253bc3cd3194
ygulve <38343976+ygulve@users.noreply.github.com>
2019-06-13 15:46:45 +0530

    Added validation on the page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormGroup, Validators, FormControl, FormBuilder } from '@angular/forms';
 import { LoginService } from '../login/login.service';
 import { Employee } from '../model/employee';
 import { Router } from '@angular/router';
@@ -17,43 +17,60 @@ export class LoginComponent implements OnInit {
   public loginPage: FormGroup;
   public header: string = "Login here";
   public _employee: Employee;
+  submitted = false;
 
-  constructor(public _loginservice: LoginService, private router: Router, private message: MessageService,
-    private eventEmitterService: EventEmitterService) { }
+  constructor(private formBuilder: FormBuilder, public _loginservice: LoginService, private router: Router, private message: MessageService,
+    private eventEmitterService: EventEmitterService, ) { }
 
   ngOnInit() {
+    localStorage.clear();
     this.initializeForm();
   }
 
   initializeForm() {
-    this.loginPage = new FormGroup({
+    this.loginPage = this.formBuilder.group({
       UserId: new FormControl('', [Validators.required]),
       Password: new FormControl('', [Validators.required])
     });
   }
 
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginPage.controls;
+  }
+
   submit(loginPage) {
     debugger;
-    var password = EncryptPassword(loginPage.value.Password, loginPage.value.UserId);
-    //loginPage.value.Password = password.toString();
+    this.submitted = true;
+    if (loginPage.valid) {
 
-    var decrypt = DecryptPassword(loginPage.value.Password, loginPage.value.UserId);
-
-    this._loginservice.login(loginPage.value).subscribe(res => {
-
-      if (res.token) {
-        localStorage.setItem('token', res.token);
-        localStorage.setItem("expires_at", JSON.stringify(res.expiresAt));
-        this.eventEmitterService.onFirstComponentButtonClick();
-        this.router.navigateByUrl('list');
+      if (loginPage.status == "INVALID") {
+        this.message.showResgistrationFailed();
       }
+      else {
 
-      if (res.error) {
-        this.message.showLoginFailed(res.error);
+        var password = EncryptPassword(loginPage.value.Password, loginPage.value.UserId);
+        //loginPage.value.Password = password.toString();
+
+        var decrypt = DecryptPassword(loginPage.value.Password, loginPage.value.UserId);
+
+        this._loginservice.login(loginPage.value).subscribe(res => {
+
+          if (res.token) {
+            localStorage.setItem('token', res.token);
+            localStorage.setItem("expires_at", JSON.stringify(res.expiresAt));
+            this.eventEmitterService.onFirstComponentButtonClick();
+            this.router.navigateByUrl('list');
+          }
+
+          if (res.error) {
+            this.message.showLoginFailed(res.error);
+          }
+        }, (err) => {
+          console.log(err)
+        });
       }
-    }, (err) => {
-      console.log(err)
-    });
+    }
   }
 
   register() {
